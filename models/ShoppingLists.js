e14a3b25b5550a861e7a7f48b52a8e64d67baaf6
gatejarr <gatejarr@gmail.com>
2024-02-13 15:37:05 -0500

    WIP; timestamps on individual items now working

--- models/ShoppingLists.js
+++ models/ShoppingLists.js
@@ -1,31 +1,5 @@
 const mongoose = require("mongoose");
 
-// const itemsSchema = new mongoose.Schema(
-//   {
-//     items: [
-//       {
-//         item: String,
-//         quantity: Number,
-//         purchased: Boolean,
-//       },
-//     ],
-//   },
-//   { timestamps: true }
-// );
-
-// const shoppingListSchema = new mongoose.Schema(
-//   {
-//     title: {
-//       type: String,
-//       required: true,
-//       maxLength: 50,
-//       minLength: 3,
-//     },
-//     items: [itemsSchema],
-//   },
-//   { timestamps: true }
-// );
-
 const shoppingListSchema = new mongoose.Schema(
   {
     title: {
@@ -34,9 +8,13 @@ const shoppingListSchema = new mongoose.Schema(
       maxLength: 50,
       minLength: 3,
     },
-    items: [
-      { item: String, quantity: Number, purchased: Boolean }, {timestamps: true },
-    ],
+    items: [{ type: new mongoose.Schema({
+      item: String,
+      quantity: Number,
+      purchased: Boolean,
+    },
+    { timestamps: true })
+  }]
   },
   { timestamps: true }
 );
